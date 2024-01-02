2145dc657833a0a45d89448de3e5500ab7ffcd3c
AnasKarakra <anaskarakra8@gmail.com>
2024-01-02 21:53:39 +0200

    version

--- app/src/main/java/com/example/ass2test2/LogInActivity.java
+++ app/src/main/java/com/example/ass2test2/LogInActivity.java
@@ -32,7 +32,7 @@ public class LogInActivity extends AppCompatActivity {
     private ArrayList<User> arraylist;
 
     //===============================================================================
-
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
